6ddc186304ee5ddcc0edde5bec5c44dc935842a5
Jean Lescure <jean@ticowebmedia.com>
2015-05-20 02:15:23 -0600

    Waiter sub-module added.

--- spec/wait_man_spec.js
+++ spec/wait_man_spec.js
@@ -1,7 +1,72 @@
 var WaitMan = require('../lib/wait-man');
 
 describe('WaitMan module', function(){
+  beforeAll(function(){
+    waitMan = new WaitMan();
+  });
+  
   it('is a constructor',function(){
     expect(typeof WaitMan).toBe('function');
+  });
+  
+  it('can be instantiated',function(){
+    expect(typeof waitMan).toBe('object');
+  });
+  
+  it('can extend objects', function(){
+    var obj_a = {"first_key": "value", "second_key": "value 2"};
+    var obj_b = {"first_key": "value 1", "third_key": "value 3"};
+    
+    waitMan.extend(obj_a, obj_b);
+    
+    expect(obj_a).toEqual({"first_key": "value 1", "second_key": "value 2", "third_key": "value 3"});
+  });
+  
+  it('has Waiter constructor',function(){
+    expect(typeof waitMan.Waiter).toBe('function');
+  });
+});
+
+describe('WaitMan.Waiter', function(){
+  beforeAll(function(){
+    waitMan = new WaitMan();
+    waiter = new waitMan.Waiter();
+    async_complete = false;
+    dummy_async = setTimeout(function(){async_complete = true}, 2000);
+  });
+  
+  it('hates not being fed a success callback function', function(){
+    var error = false;
+    try{
+      waiter.tick();
+    }catch(err){
+      error = true;
+    }
+    
+    expect(error).toBeTruthy();
+  });
+  
+  it('hates not being fed a test function', function(){
+    var error = false;
+    try{
+      waiter.test();
+    }catch(err){
+      error = true;
+    }
+    
+    expect(error).toBeTruthy();
+  });
+  
+  it('waits for test function to return true', function(done){
+    waiter.test = function(){ return async_complete; };
+    
+    var async_uid = '';
+    var beganAt = +new Date();
+    waiter.tick(function(uid){
+      var elapsed = +new Date() - beganAt;
+      expect(elapsed).toBeGreaterThan(2000);
+      expect(uid).toEqual(waiter.uid);
+      done();
+    });
   });
 });
